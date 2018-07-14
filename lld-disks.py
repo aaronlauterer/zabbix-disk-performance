ccba11a06e1ca5c4a4bf51ce7ac7962e4df5427d
Taha Jahangir <mtjahangir@gmail.com>
2018-07-14 11:01:45 +0430

    Better regex filter (support nvme disk)

--- lld-disks.py
+++ lld-disks.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     # Iterate over all block devices, but ignore them if they are in the
     # skippable set
     # skip /dev/sda1 in favour of /dev/sda
-    skippable = (r'^(sr|loop|ram)', r'\d')
+    # skip: sda1/nvme0n1p1/xvdg1  , keep: sda/nvme0n1/xvdg
+    skippable = (r'^(sr|loop|ram|fd)', r'^(sd\w|hd\w|nvme\d+n\d+p|xvd\w|vd\w)\d+$')
     devices = (device for device in os.listdir("/sys/class/block")
                if not any(re.search(ignore, device) for ignore in skippable))
     data = [{"{#DEVICENAME}": device} for device in devices]
