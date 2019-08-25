d5d83ebc9323b25378402460eeee369911165210
Aaron Lauterer <aaron@lauterer.at>
2019-08-25 14:52:13 +0200

    Add filter for ZVOL partitions

--- lld-disks.py
+++ lld-disks.py
@@ -8,8 +8,14 @@ if __name__ == "__main__":
     # skippable set
     # skip /dev/sda1 in favour of /dev/sda
     # skip: sda1/nvme0n1p1/xvdg1  , keep: sda/nvme0n1/xvdg
-    skippable = (r'^(sr|loop|ram|fd)', r'^(sd\w|hd\w|nvme\d+n\d+p|xvd\w|vd\w)\d+$')
-    devices = (device for device in os.listdir("/sys/class/block")
-               if not any(re.search(ignore, device) for ignore in skippable))
+    skippable = (
+        r"^(sr|loop|ram|fd)",
+        r"^(sd\w|hd\w|nvme\d+n\d+p|zd\d+p|xvd\w|vd\w)\d+$",
+    )
+    devices = (
+        device
+        for device in os.listdir("/sys/class/block")
+        if not any(re.search(ignore, device) for ignore in skippable)
+    )
     data = [{"{#DEVICENAME}": device} for device in devices]
     print(json.dumps({"data": data}, indent=4))
